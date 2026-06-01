c4d0a7e06d9b34c96d438cddb61837bb445af83a
fantan5911 <arthur40rutr@gmail.com>
2026-06-01 17:03:53 +0500

    Params changes

--- frontend/src/app/(public)/smartphone/id/[id]/page.tsx
+++ frontend/src/app/(public)/smartphone/id/[id]/page.tsx
@@ -2,8 +2,11 @@ import SmartphoneId from "@/components/SmartphoneId";
 import { PageLoading } from "@/components/UI/PageLoading";
 import { Suspense } from "react";
 
+type Params = {
+    id: string;
+}
 
-export default function SmartphoneIdPage({params}: {params: {id: string}}) {
+export default function SmartphoneIdPage({params}: {params: Promise<Params>}) {
     return (
         <Suspense fallback={<PageLoading />}>
             <SmartphoneId params={Promise.resolve(params)} />
